72d110afb8ffab0b6bb76dc2b0729d5619f65500
Rupesh Deshmukh <rupesh201098@gmail.com>
2021-06-03 00:58:55 -0400

    code

---
+++ main.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, LSTM
+
+# Load data
+company = 'FB'
+
+start = dt.datetime(2012, 1, 1)
+end = dt.datetime(2020, 1, 1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+# Prepare Data
+scalar = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scalar.fit_transform(data['Close'].values.reshape(-1, 1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x - prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train = np.array(x_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = np.array(y_train)
+
+# Uncomment lines below to re-train the model
+'''
+# Building the Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+model.save('models/model.h5')
+'''
+
+model = load_model('models/model.h5')
+
+# Load Test Data
+
+test_start = dt.datetime(2020, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']))
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scalar.transform(model_inputs)
+
+# Make Predictions on Test Data
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x - prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scalar.inverse_transform(predicted_prices)
+
+# Plot Predictions
+
+plt.plot(actual_prices, color='black', label=f'Actual {company} prices')
+plt.plot(predicted_prices, color='green', label=f'Predicted {company} prices')
+plt.title(f'{company} Share Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
